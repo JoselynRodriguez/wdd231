0e1a4bfb73aaa2f2a179c40082dc20761d457319
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-06 16:35:13 -0500

    Header, nav, and footer ready

---
+++ scripts/home.js
@@ -0,0 +1,14 @@
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
