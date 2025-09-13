f674f4a17f7645d5943c6a0b98f439f5630f2bd3
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-09-13 18:53:06 -0500

    Updating footer and header

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,6 @@
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
