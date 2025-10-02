82f29f0bb9a05d77e86c08dc29b562b5d44e9169
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-10-02 18:53:16 -0500

    siteplan

--- scripts/siteplan.js
+++ scripts/siteplan.js
@@ -7,10 +7,3 @@ lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
 const navButton = document.querySelector('#navbutton');
 const navBar = document.querySelector('#navbar');
-
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-    navBar.classList.toggle('show');
-});
-
-
