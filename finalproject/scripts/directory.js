1157209783e5d9c06de11afee4afacb579c54902
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-10-16 18:50:30 -0500

    submit

--- finalproject/scripts/directory.js
+++ finalproject/scripts/directory.js
@@ -12,3 +12,46 @@ yearSpan.textContent = currentYear;
 
 const lastModified = document.getElementById("lastModified");
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const gridBtn = document.querySelector('#grid');
+const listBtn = document.querySelector('#list');
+const membersSection = document.querySelector('#members');
+
+gridBtn.addEventListener('click', () => {
+  membersSection.classList.add('grid');
+  membersSection.classList.remove('list');
+});
+
+listBtn.addEventListener('click', () => {
+  membersSection.classList.add('list');
+  membersSection.classList.remove('grid');
+});
+
+const url = 'data/members.json'; 
+
+async function getMembers() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data);
+
+}
+
+function displayMembers(members) {
+  members.forEach(member => {
+    const card = document.createElement('div');
+    card.classList.add('member-card');
+
+    card.innerHTML = `
+      <div class="member-row">
+        <div class="name"><strong>${member.name}</strong></div>
+        <div class="birth"><strong>Birth Date:</strong> ${member.birthDate}</div>
+        <div class="age"><strong>Age:</strong> ${member.age}</div>
+        <div class="phone"><strong>Phone:</strong> ${member.phone}</div>
+        <div class="email"><strong>Email:</strong> ${member.email}</div>
+      </div>
+    `;
+    membersSection.appendChild(card);
+  });
+}
+
+getMembers();
--- finalproject/scripts/home.js
+++ finalproject/scripts/home.js
@@ -12,3 +12,45 @@ yearSpan.textContent = currentYear;
 
 const lastModified = document.getElementById("lastModified");
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const myLat = "12.0467";
+const myLon = "77.0431";
+const myKey = "dce3be4c106bea03439971ce1c3d3201";
+
+const myURL = 'api.openweathermap.org/data/2.5/forecast?lat={myLat}&lon={myLon}&appid={mykey}';
+
+
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); 
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;C`;
+
+  const iconCode = data.weather[0].icon;
+  const iconsrc = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  let desc = data.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
+
