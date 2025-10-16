73f2a3ce9739e291a527571a18cef865bb2fa3d9
JoselynRodriguez <jrosmerirodriguezr@gmail.com>
2025-10-16 16:04:11 -0500

    update

---
+++ finalproject/scripts/join.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector('#navbutton');
+const navBar = document.querySelector('#navbar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
